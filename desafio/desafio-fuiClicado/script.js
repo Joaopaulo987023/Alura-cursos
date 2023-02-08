c6df62ceb4113150fb2908ee9291a514e5fa30f4
João Paulo <46024524+Joaopaulo987023@users.noreply.github.com>
2023-02-08 12:39:56 -0300

    Fiz mais um desfio

---
+++ desafio/desafio-fuiClicado/script.js
@@ -0,0 +1,13 @@
+const botao = document.querySelector('#calcular');
+
+
+
+
+botao.addEventListener('click', ()=>{
+    
+    const res = document.querySelector('.resultado');
+    botao.innerHTML='Fui clicado';
+    res.innerHTML = 'Fui clicado';
+
+    
+})
